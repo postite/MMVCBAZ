abbd50f43bf1ebaf9c19609c3ec41a31d4d979f0
postite <blog@postite.com>
2013-08-25 15:47:17 +0200

    added currentApp

--- com/postite/App.hx
+++ com/postite/App.hx
@@ -8,7 +8,7 @@ class App
 	*/
 	public static function main()
 	{
-		var view = new com.postite.app.AppView();
-		var context = new com.postite.app.AppContext(view);
+		var view = new com.postite.::currentApp::.app.AppView();
+		var context = new com.postite.::currentApp::.app.AppContext(view);
 	}
 }
